745b155e3e1064399baa975d094cc028517378e7
ishitaka-shuri <s.ishitaka@wreath-ent.co.jp>
2026-08-10 10:44:06 +0900

    0810石高スポンサーリンク追加

--- page-sponsor.php
+++ page-sponsor.php
@@ -15,49 +15,164 @@ if (!defined('ABSPATH')) exit;
                 </div>
                 <div class="p-sponsor_inner_group">
                     <h3>PLATINUM PARTNER</h3>
-                    <div class="p-sponsor_inner_logo">
-                        <img src="<?= get_image_path('sp', 'sponsor01.jpg'); ?>" alt="企業ロゴ">
-                        <img src="<?= get_image_path('sp', 'sponsor02.jpg'); ?>" alt="企業ロゴ">
-                        <img src="<?= get_image_path('sp', 'sponsor03.jpg'); ?>" alt="企業ロゴ">
-                        <img src="<?= get_image_path('sp', 'sponsor04.jpg'); ?>" alt="企業ロゴ">
-                    </div>
+                    <ul class="p-sponsor_inner_logo">
+                        <li class="p-sponsor_inner_item">
+                            <a class="p-sponsor_inner_link" href="https://genova.co.jp/" target="_blank">
+                                <p class="p-sponsor_inner_name">WEBマーケティング</p>
+                                <img src="<?= get_image_path('sp', 'sponsor01.jpg'); ?>" alt="企業ロゴ">
+                            </a>
+                        </li>
+                        <li class="p-sponsor_inner_item">
+                            <a class="p-sponsor_inner_link" href="https://www.medical-net.com/" target="_blank">
+                                <p class="p-sponsor_inner_name">WEBマーケティング</p>
+                                <img src="<?= get_image_path('sp', 'sponsor02.jpg'); ?>" alt="企業ロゴ">
+                            </a>
+                        </li>
+                        <li class="p-sponsor_inner_item">
+                            <a class="p-sponsor_inner_link" href="https://corp.4dx.jp/" target="_blank">
+                                <p class="p-sponsor_inner_name">口腔内スキャナー　矯正技工</p>
+                                <img src="<?= get_image_path('sp', 'sponsor03.jpg'); ?>" alt="企業ロゴ">
+                            </a>
+                        </li>
+                        <li class="p-sponsor_inner_item">
+                            <a class="p-sponsor_inner_link" href="https://dentalx.jp/" target="_blank">
+                                <p class="p-sponsor_inner_name">レセコン</p>
+                                <img src="<?= get_image_path('sp', 'sponsor04.jpg'); ?>" alt="企業ロゴ">
+                            </a>
+                        </li>
+                    </ul>
                 </div>
 
                 <div class="p-sponsor_inner_group">
                     <h3>GOLD PARTNER</h3>
-                    <div class="p-sponsor_inner_logo">
-                        <img src="<?= get_image_path('sp', 'sponsor05.jpg'); ?>" alt="企業ロゴ">
-                        <img src="<?= get_image_path('sp', 'sponsor06.jpg'); ?>" alt="企業ロゴ">
-                        <img src="<?= get_image_path('sp', 'sponsor07.jpg'); ?>" alt="企業ロゴ">
-                        <img src="<?= get_image_path('sp', 'sponsor08.jpg'); ?>" alt="企業ロゴ">
-                    </div>
+                    <ul class="p-sponsor_inner_logo">
+                        <li class="p-sponsor_inner_item">
+                            <a class="p-sponsor_inner_link" href="https://smiletru.jp/ptmain/" target="_blank">
+                                <p class="p-sponsor_inner_name">矯正技工</p>
+                                <img src="<?= get_image_path('sp', 'sponsor05.jpg'); ?>" alt="企業ロゴ">
+                            </a>
+                        </li>
+                        <li class="p-sponsor_inner_item">
+                            <a class="p-sponsor_inner_link" href="https://branding-bd.co.jp/" target="_blank">
+                                <p class="p-sponsor_inner_name">野立て看板制作</p>
+                                <img src="<?= get_image_path('sp', 'sponsor06.jpg'); ?>" alt="企業ロゴ">
+                            </a>
+                        </li>
+                        <li class="p-sponsor_inner_item">
+                            <a class="p-sponsor_inner_link" href="https://dz-life.co.jp/" target="_blank">
+                                <p class="p-sponsor_inner_name">節税・資産形成</p>
+                                <img src="<?= get_image_path('sp', 'sponsor07.jpg'); ?>" alt="企業ロゴ">
+                            </a>
+                        </li>
+                        <li class="p-sponsor_inner_item">
+                            <a class="p-sponsor_inner_link" href="https://wreath-ent.co.jp/" target="_blank">
+                                <p class="p-sponsor_inner_name">ホームページ制作</p>
+                                <img src="<?= get_image_path('sp', 'sponsor08.jpg'); ?>" alt="企業ロゴ">
+                            </a>
+                        </li>
+                    </ul>
                 </div>
 
                 <div class="p-sponsor_inner_group">
                     <h3>SILVER PARTNER</h3>
-                    <div class="p-sponsor_inner_logo">
-                        <img src="<?= get_image_path('sp', 'sponsor09.jpg'); ?>" alt="企業ロゴ">
-                        <img src="<?= get_image_path('sp', 'sponsor10.jpg'); ?>" alt="企業ロゴ">
-                        <img src="<?= get_image_path('sp', 'sponsor11.jpg'); ?>" alt="企業ロゴ">
-                        <img src="<?= get_image_path('sp', 'sponsor12.jpg'); ?>" alt="企業ロゴ">
-                        <img src="<?= get_image_path('sp', 'sponsor13.jpg'); ?>" alt="企業ロゴ">
-                        <img src="<?= get_image_path('sp', 'sponsor14.jpg'); ?>" alt="企業ロゴ">
-                        <img src="<?= get_image_path('sp', 'sponsor15.jpg'); ?>" alt="企業ロゴ">
-                        <img src="<?= get_image_path('sp', 'sponsor16.jpg'); ?>" alt="企業ロゴ">
-                        <img src="<?= get_image_path('sp', 'sponsor17.jpg'); ?>" alt="企業ロゴ">
-                        <img src="<?= get_image_path('sp', 'sponsor18.jpg'); ?>" alt="企業ロゴ">
-                        <img src="<?= get_image_path('sp', 'sponsor21.jpg'); ?>" alt="企業ロゴ">
-                        <img src="<?= get_image_path('sp', 'sponsor22.jpg'); ?>" alt="企業ロゴ">
-                        <img src="<?= get_image_path('sp', 'sponsor23.jpg'); ?>" alt="企業ロゴ">
-                    </div>
+                    <ul class="p-sponsor_inner_logo">
+                        <li class="p-sponsor_inner_item">
+                            <a class="p-sponsor_inner_link" href="https://www.medi-art.co.jp/" target="_blank">
+                                <p class="p-sponsor_inner_name">デジタルサイネージ</p>
+                                <img src="<?= get_image_path('sp', 'sponsor09.jpg'); ?>" alt="企業ロゴ">
+                            </a>
+                        </li>
+                        <li class="p-sponsor_inner_item">
+                            <a class="p-sponsor_inner_link" href="https://www.medicalshift.co.jp/" target="_blank">
+                                <p class="p-sponsor_inner_name">WEBマーケティング</p>
+                                <img src="<?= get_image_path('sp', 'sponsor10.jpg'); ?>" alt="企業ロゴ">
+                            </a>
+                        </li>
+                        <li class="p-sponsor_inner_item">
+                            <a class="p-sponsor_inner_link" href="https://synergy-gunma.com/" target="_blank">
+                                <p class="p-sponsor_inner_name">人財採用代行</p>
+                                <img src="<?= get_image_path('sp', 'sponsor11.jpg'); ?>" alt="企業ロゴ">
+                            </a>
+                        </li>
+                        <li class="p-sponsor_inner_item">
+                            <a class="p-sponsor_inner_link" href="https://yourc.co.jp/" target="_blank">
+                                <p class="p-sponsor_inner_name">ホワイトニング</p>
+                                <img src="<?= get_image_path('sp', 'sponsor12.jpg'); ?>" alt="企業ロゴ">
+                            </a>
+                        </li>
+                        <li class="p-sponsor_inner_item">
+                            <a class="p-sponsor_inner_link" href="https://van-cleef.jp/" target="_blank">
+                                <p class="p-sponsor_inner_name">ホワイトニング</p>
+                                <img src="<?= get_image_path('sp', 'sponsor13.jpg'); ?>" alt="企業ロゴ">
+                            </a>
+                        </li>
+                        <li class="p-sponsor_inner_item">
+                            <a class="p-sponsor_inner_link" href="https://shadeplus.jp/" target="_blank">
+                                <p class="p-sponsor_inner_name">ジルコニアディスク販売</p>
+                                <img src="<?= get_image_path('sp', 'sponsor14.jpg'); ?>" alt="企業ロゴ">
+                            </a>
+                        </li>
+                        <li class="p-sponsor_inner_item">
+                            <a class="p-sponsor_inner_link" href="https://www.luxurycard.co.jp/" target="_blank">
+                                <p class="p-sponsor_inner_name">クレジットカード</p>
+                                <img src="<?= get_image_path('sp', 'sponsor15.jpg'); ?>" alt="企業ロゴ">
+                            </a>
+                        </li>
+                        <li class="p-sponsor_inner_item">
+                            <a class="p-sponsor_inner_link" href="https://www.aeroserv.co.jp/" target="_blank">
+                                <p class="p-sponsor_inner_name">業務用空気清浄装置</p>
+                                <img src="<?= get_image_path('sp', 'sponsor16.jpg'); ?>" alt="企業ロゴ">
+                            </a>
+                        </li>
+                        <li class="p-sponsor_inner_item">
+                            <a class="p-sponsor_inner_link" href="https://midg.jp/matching/pbm.html" target="_blank">
+                                <p class="p-sponsor_inner_name">光加速矯正装置</p>
+                                <img src="<?= get_image_path('sp', 'sponsor17.jpg'); ?>" alt="企業ロゴ">
+                            </a>
+                        </li>
+                        <li class="p-sponsor_inner_item">
+                            <a class="p-sponsor_inner_link" href="https://www.guidedent.net/" target="_blank">
+                                <p class="p-sponsor_inner_name">インプラント保証事業</p>
+                                <img src="<?= get_image_path('sp', 'sponsor18.jpg'); ?>" alt="企業ロゴ">
+                            </a>
+                        </li>
+                        <li class="p-sponsor_inner_item">
+                            <a class="p-sponsor_inner_link" href="https://www.soeasy.co.jp/" target="_blank">
+                                <p class="p-sponsor_inner_name">動画マニュアル</p>
+                                <img src="<?= get_image_path('sp', 'sponsor21.jpg'); ?>" alt="企業ロゴ">
+                            </a>
+                        </li>
+                        <li class="p-sponsor_inner_item">
+                            <a class="p-sponsor_inner_link" href="https://www.eposcard.co.jp/index.html" target="_blank">
+                                <p class="p-sponsor_inner_name">ローン・決算関連</p>
+                                <img src="<?= get_image_path('sp', 'sponsor22.jpg'); ?>" alt="企業ロゴ">
+                            </a>
+                        </li>
+                        <li class="p-sponsor_inner_item">
+                            <a class="p-sponsor_inner_link" href="https://www.readlead.co.jp/" target="_blank">
+                                <p class="p-sponsor_inner_name">デジタルサイネージ</p>
+                                <img src="<?= get_image_path('sp', 'sponsor23.jpg'); ?>" alt="企業ロゴ">
+                            </a>
+                        </li>
+                    </ul>
                 </div>
 
                 <div class="p-sponsor_inner_group">
                     <h3>BRONZE PARTNER</h3>
-                    <div class="p-sponsor_inner_logo">
-                        <img src="<?= get_image_path('sp', 'sponsor19.jpg'); ?>" alt="企業ロゴ">
-                        <img src="<?= get_image_path('sp', 'sponsor20.jpg'); ?>" alt="企業ロゴ">
-                    </div>
+                    <ul class="p-sponsor_inner_logo">
+                         <li class="p-sponsor_inner_item">
+                            <a class="p-sponsor_inner_link" href="https://www.geistlich.com/ja-jp/" target="_blank">
+                                <p class="p-sponsor_inner_name">インプラント　骨補填剤</p>
+                                <img src="<?= get_image_path('sp', 'sponsor19.jpg'); ?>" alt="企業ロゴ">
+                            </a>
+                        </li>
+                        <li class="p-sponsor_inner_item">
+                            <a class="p-sponsor_inner_link" href="http://www.ydm.co.jp/" target="_blank">
+                                <p class="p-sponsor_inner_name">スケーラー・インツルメント</p>
+                                <img src="<?= get_image_path('sp', 'sponsor20.jpg'); ?>" alt="企業ロゴ">
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </div>
     </section>
